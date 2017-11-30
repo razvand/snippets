6a5d593c90fdd7c70f21d5c49b5b1ee12d22c361
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-11-30 13:44:31 +0200

    config: Add (package-initialize) for Emacs

--- config/emacs/emacs.el
+++ config/emacs/emacs.el
@@ -2,6 +2,13 @@
 ;; Editing configuration
 ;;
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq global-font-lock-mode t)
 (setq x-select-enable-clipboard t)
 (setq c-default-style "linux")
