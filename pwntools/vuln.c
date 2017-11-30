0a3f3f4677d64ff47755e396f99f7c873af1d98c
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-11-30 12:01:27 +0200

    Add target for pwntools test cases

---
+++ pwntools/vuln.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* global test variable */
+static size_t g = 0x42424242;
+
+static void warcraft(void)
+{
+	puts("chieftain");
+}
+
+static void diablo(size_t a, size_t b)
+{
+	if (a == 0x12345678 && b == 0xaabbccdd)
+		puts("worldstone");
+}
+
+static void starcraft(void)
+{
+	if (g == 0xc001face)
+		puts("koprulu");
+}
+
+static void reader(void)
+{
+	char buffer[64];
+
+	printf("gimme message: ");
+	fgets(buffer, 128, stdin);
+	printf("hello, %s\n", buffer);
+}
+
+int main(void)
+{
+	puts("hello, blizzard!");
+
+	reader();
+
+	return 0;
+}
