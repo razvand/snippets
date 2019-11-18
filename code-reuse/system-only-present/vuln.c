5087a2423d5d70281f2ba3594b4063383999ae43
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-11-18 19:55:57 +0200

    Add code-reuse pwn-based exploits

---
+++ code-reuse/system-only-present/vuln.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void hidden(void)
+{
+	system("ls");
+}
+
+static void reader(void)
+{
+	char buffer[32];
+
+	fgets(buffer, 128, stdin);
+}
+
+int main(void)
+{
+	puts("Hello");
+	reader();
+	return 0;
+}
